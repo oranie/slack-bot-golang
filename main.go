e9fa1f52360ea1efadccda9ad28323fbed496bcf
成田 俊 <narita_takashi@cyberagent.co.jp>
2015-01-16 18:11:05 +0900

    make it better

--- main.go
+++ main.go
@@ -1,83 +1,42 @@
 package main
 
 import (
-	"net/url"
+	"github.com/gin-gonic/gin"
 	"log"
-	"net/http"
-	"encoding/json"
-	"os/user"
 	"os"
-	"errors"
 )
 
-type SlackMsg struct {
-	Channel string `json:"channel"`
-	Username string `json:"username,omitempty"`
-	Text string `json:"text"`
-	Parse string `json:"parse"`
-	IconEmoji string `json:"icon_emoji,omitempty"`
-}
-
-type Config struct {
-	WebhookUrl string `json:"webhook_url"`
-	Channel string `json:"channel"`
-	Username string `json:"username"`
-	IconEmoji string `json:"iconemoji"`
-}
-
-func ReadConfig() (*Config, error) {
-	homeDir := ""
-	usr, err := user.Current()
-	if err == nil {
-		homeDir = usr.HomeDir
-	}
-
-	for _, path := range []string{ homeDir + "/.slack-bot-golang.cfg", "./slack-bot-golang.cfg"} {
-		file, err := os.Open(path)
-		if os.IsNotExist(err) {
-			continue
-		}
-		if err != nil {
-			log.Println(err)
-			return nil, err
-		}
-
-		json.NewDecoder(file)
-		conf := Config{}
-		err = json.NewDecoder(file).Decode(&conf)
-		if err != nil {
-			log.Println("config json decode",&conf,err)
-			return nil, err
-		}
-		return &conf, nil
-	}
-
-	return nil, errors.New("Config file not found")
-}
-
-func SlackPost(cfg *Config) error{
-
-	var data SlackMsg
-	data.Channel = cfg.Channel
-	data.Username = cfg.Username
-	data.IconEmoji = cfg.IconEmoji
-	data.Parse = "full"
-	data.Text = "ちょっとソースを書き換えたもので"
-	jsonData,err := json.Marshal(data)
-	log.Println(string(jsonData),err)
-
-	WebhookUrl := cfg.WebhookUrl
-
-	//log.Println(data,string(jsonData))
-
-	resp, err := http.PostForm(WebhookUrl, url.Values{"payload": {string(jsonData)}})
-	log.Println(resp.Status,resp.Body," : ",err)
-	return err
-}
 
 func main() {
-	cfg, err := ReadConfig()
-	log.Println(cfg,err)
-	err = SlackPost(cfg)
-	log.Println(err)
+	server := gin.New()
+	server.Use(gin.Recovery())
+
+	api := server.Group("/v1")
+	{
+		slack := api.Group("/slack")
+		{
+			slack.POST("/inbound",PostDataSTDOut)
+		}
+	}
+	server.Run(":"+os.Getenv("PORT"))
+}
+
+type slackHookMesage struct {
+	Token string  `json:"token"`
+	TeamId string  `json:"team_id"`
+	ChannelId string  `json:"channel_id"`
+	ChannelName string  `json:"channel_name"`
+	Timestamp float32  `json:"timestamp"`
+	UserId  string  `json:"user_id"`
+	UserName string  `json:"user_name"`
+	Text string  `json:"text"`
+	TriggerWord string  `json:"trigger_word"`
+}
+
+
+func PostDataSTDOut(g *gin.Context){
+	var message slackHookMesage
+	g.Bind(&message)
+
+	log.Println(message)
 }

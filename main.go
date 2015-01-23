b2f3930b575b5c8d727d87ae0471e202ec2ac65a
成田 俊 <narita_takashi@cyberagent.co.jp>
2015-01-23 17:12:44 +0900

    go-gin remove

--- main.go
+++ main.go
@@ -2,68 +2,40 @@ package main
 
 import (
 	"log"
-	"bytes"
-	"github.com/gin-gonic/gin"
-	"os"
+	"net/http"
 )
 
 type slackHookMessage struct {
-	Token string  `form:"token"`
-	TeamId string  `form:"team_id"`
-	ChannelId string  `form:"channel_id"`
-	ChannelName string  `form:"channel_name"`
-	Timestamp float32  `form:"timestamp"`
-	UserId  string  `form:"user_id"`
-	UserName string  `form:"user_name"`
-	Text string  `form:"text"`
-	TriggerWord string  `form:"trigger_word"`
+	Token string  `json:"token"`
+	TeamId string  `json:"team_id"`
+	ChannelId string  `json:"channel_id"`
+	ChannelName string  `json:"channel_name"`
+	Timestamp string  `json:"timestamp"`
+	UserId  string  `json:"user_id"`
+	UserName string  `json:"user_name"`
+	Text string  `json:"text"`
+	TriggerWord string  `json:"trigger_word"`
 }
 
-/*
-func Serve(w http.ResponseWriter, r *http.Request) {
-	text := r.FormValue("text")
 
-	log.Println(text)
+func BindSlackData(w http.ResponseWriter, r *http.Request)  {
+	var postData slackHookMessage
+	postData.Token = r.FormValue("token")
+	postData.TeamId = r.FormValue("team_id")
+	postData.ChannelId = r.FormValue("channel_id")
+	postData.ChannelName = r.FormValue("channel_name")
+	postData.Timestamp = r.FormValue("timestamp")
+	postData.UserId = r.FormValue("user_id")
+	postData.UserName = r.FormValue("user_name")
+	postData.Text = r.FormValue("text")
+	postData.TriggerWord = r.FormValue("trigger_word")
+
+	log.Println(postData)
+
 }
 
 
 func main() {
-	http.HandleFunc("/inbound", Serve)
+	http.HandleFunc("/v1/slack/inbound", BindSlackData)
 	http.ListenAndServe(":8888", nil)
 }
-*/
-
-
-
-func main() {
-	server := gin.New()
-	server.Use(gin.Recovery())
-
-	api := server.Group("/v1")
-	{
-		slack := api.Group("/slack")
-		{
-			slack.POST("/inbound",PostDataSTDOut)
-		}
-	}
-	server.Run(":"+os.Getenv("PORT"))
-	//server.Run(":8888")
-}
-
-func PostDataSTDOut(g *gin.Context){
-	bufbody := new(bytes.Buffer)
-	bufbody.ReadFrom(g.Request.Body)
-	body := bufbody.String()
-
-	var message slackHookMessage
-
-	//g.BindWith(&message,binding.Form)
-	//g.Bind(&message)
-	log.Println(message.Text)
-	log.Println(message)
-
-	log.Println(g.Request.Header)
-
-	log.Println(body)
-}
-

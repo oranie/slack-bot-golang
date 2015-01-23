75f22f64b1d75e5dfc12a4ab1f282a530ae5f58f
成田 俊 <narita_takashi@cyberagent.co.jp>
2015-01-23 13:40:18 +0900

    header log add

--- main.go
+++ main.go
@@ -1,12 +1,37 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"log"
-	"os"
 	"bytes"
-	"github.com/gin-gonic/gin/binding"
+	"github.com/gin-gonic/gin"
 )
+
+type slackHookMessage struct {
+	Token string  `form:"token"`
+	TeamId string  `form:"team_id"`
+	ChannelId string  `form:"channel_id"`
+	ChannelName string  `form:"channel_name"`
+	Timestamp float32  `form:"timestamp"`
+	UserId  string  `form:"user_id"`
+	UserName string  `form:"user_name"`
+	Text string  `form:"text"`
+	TriggerWord string  `form:"trigger_word"`
+}
+
+/*
+func Serve(w http.ResponseWriter, r *http.Request) {
+	text := r.FormValue("text")
+
+	log.Println(text)
+}
+
+
+func main() {
+	http.HandleFunc("/inbound", Serve)
+	http.ListenAndServe(":8888", nil)
+}
+*/
+
 
 
 func main() {
@@ -20,31 +45,24 @@ func main() {
 			slack.POST("/inbound",PostDataSTDOut)
 		}
 	}
-	server.Run(":"+os.Getenv("PORT"))
+	//server.Run(":"+os.Getenv("PORT"))
+	server.Run(":8888")
 }
-
-type slackHookMesage struct {
-	Token string  `form:"token"`
-	TeamId string  `form:"team_id"`
-	ChannelId string  `form:"channel_id"`
-	ChannelName string  `form:"channel_name"`
-	Timestamp float32  `form:"timestamp"`
-	UserId  string  `form:"user_id"`
-	UserName string  `form:"user_name"`
-	Text string  `form:"text"`
-	TriggerWord string  `form:"trigger_word"`
-}
-
 
 func PostDataSTDOut(g *gin.Context){
-	var message slackHookMesage
 	bufbody := new(bytes.Buffer)
 	bufbody.ReadFrom(g.Request.Body)
 	body := bufbody.String()
 
-	g.BindWith(&message,binding.Form)
+	var message slackHookMessage
+
+	//g.BindWith(&message,binding.Form)
+	//g.Bind(&message)
 	log.Println(message.Text)
 	log.Println(message)
 
+	log.Println(g.Request.Header)
+
 	log.Println(body)
 }
+
